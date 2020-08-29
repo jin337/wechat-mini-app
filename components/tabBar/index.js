998ba4d646d3838b680d8eb7570d62faed5c2e57
You <you9009@foxmail.com>
2020-08-29 13:55:57 +0800

    first commit

---
+++ components/tabBar/index.js
@@ -0,0 +1,72 @@
+// components/tabBar/index.js
+const app = getApp()
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    selected: {
+      type: String | Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    marBotton: 0,
+    barList: [
+      {
+        title: "Home",
+        path: "/pages/Home/index",
+        selectIcon: 'https://github-1253149949.cos.ap-shanghai.myqcloud.com/wechat_icon/home.png',
+        icon: 'https://github-1253149949.cos.ap-shanghai.myqcloud.com/wechat_icon/home%202.png',
+      },
+      {
+        title: "Repositories",
+        path: "/pages/Repositories/index",
+        selectIcon: 'https://github-1253149949.cos.ap-shanghai.myqcloud.com/wechat_icon/display-code.png',
+        icon: 'https://github-1253149949.cos.ap-shanghai.myqcloud.com/wechat_icon/display-code%202.png',
+      },
+      {
+        title: "User",
+        path: "/pages/User/index",
+        selectIcon: 'https://github-1253149949.cos.ap-shanghai.myqcloud.com/wechat_icon/account.png',
+        icon: 'https://github-1253149949.cos.ap-shanghai.myqcloud.com/wechat_icon/account%202.png',
+      },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+
+  /**
+   * 生命周期
+   */
+  lifetimes: {
+    attached () {
+      let _this = this
+      // 高度
+      if (app.globalData.iphoneX) {
+        _this.setData({
+          marBotton: 24
+        })
+      }
+      // 当前tab
+      let barList = _this.data.barList
+      let selected = _this.data.selected
+      for (let i = 0; i < barList.length; i++) {
+        const element = barList[i];
+        delete element.active
+      }
+      let index = Number(selected)
+      barList[index].active = "bar-active"
+      _this.setData({
+        barList
+      })
+    }
+  }
+})
---
+++ pages/User/index.js
@@ -0,0 +1,117 @@
+// pages/User/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    User: {
+      login: "solomonxie",
+      id: 14041622,
+      node_id: "MDQ6VXNlcjE0MDQxNjIy",
+      avatar_url: "https://avatars2.githubusercontent.com/u/14041622?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/solomonxie",
+      html_url: "https://github.com/solomonxie",
+      followers_url: "https://api.github.com/users/solomonxie/followers",
+      following_url: "https://api.github.com/users/solomonxie/following{/other_user}",
+      gists_url: "https://api.github.com/users/solomonxie/gists{/gist_id}",
+      starred_url: "https://api.github.com/users/solomonxie/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/solomonxie/subscriptions",
+      organizations_url: "https://api.github.com/users/solomonxie/orgs",
+      repos_url: "https://api.github.com/users/solomonxie/repos",
+      events_url: "https://api.github.com/users/solomonxie/events{/privacy}",
+      received_events_url: "https://api.github.com/users/solomonxie/received_events",
+      type: "User",
+      site_admin: false,
+      name: "Solomon Xie",
+      company: null,
+      blog: "https://solomonxie.github.io",
+      location: "Melbourne, Australia",
+      email: null,
+      hireable: true,
+      bio: "Jesus follower, Yankees fan, Casual Geek, Otaku, NFS Racer.",
+      twitter_username: null,
+      public_repos: 49,
+      public_gists: 0,
+      followers: 31,
+      following: 8,
+      created_at: "2015-08-30T14:44:59Z",
+      updated_at: "2020-08-18T14:41:26Z"
+    }
+  },
+
+  /**
+   * 自定义事件
+   */
+  init (options) {
+    this.getUser(options.login)
+  },
+
+  getUser (key) {
+    // wx.request({
+    //   url: 'https://api.github.com/users/' + key,
+    //   header: { 'content-type': 'application/json' },
+    //   success (res) {
+    //     console.log(res.data)
+    //   }
+    // })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.init({ login: 'youux' })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
